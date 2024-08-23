4d6a95d776ff7875a4ce685d710ede7e24a88dd4
ejum01 <ejum0713@gmail.com>
2024-08-23 16:42:33 +0900

    test

--- src/main/java/com/ysmeta/lostark/controller/HomeController.java
+++ src/main/java/com/ysmeta/lostark/controller/HomeController.java
@@ -4,6 +4,7 @@ import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -15,6 +16,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
  */
 @Controller
 public class HomeController {
+
+    @ModelAttribute("loggedIn")
+    public boolean addLoggedInAttribute(HttpSession session) {
+        return session.getAttribute("user") != null;
+    }
 
     @RequestMapping("/")
     public String home (HttpSession session, Model model) {
